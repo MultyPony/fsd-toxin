e0ee07cf08ede58f3dd0965288c189419b804878
Ivan Meshcheryakov <meshcheryakovvrn@gmail.com>
2020-08-14 14:35:12 +0300

    Add litle fixes

--- src/ui-kit.js
+++ src/ui-kit.js
@@ -3,3 +3,4 @@ import './ui-kit.scss';
 const dropdown = require('./common.blocks/dropdown/dropdown.js');
 const likeButton = require('./common.blocks/like-button/like-button.js');
 const rangeSlider = require('./common.blocks/range-slider/range-slider.js');
+const extCheckList = require('./common.blocks/ext-checkbox-list/ext-checkbox-list.js');
