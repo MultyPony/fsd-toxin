28144c0bb7e3daafd54cc59be78f3522c072c922
Ivan Meshcheryakov <meshcheryakovvrn@gmail.com>
2020-08-24 16:53:24 +0300

    Add the functionality of the plus button

--- src/common.blocks/dropdown/dropdown.js
+++ src/common.blocks/dropdown/dropdown.js
@@ -1,7 +1,18 @@
 let dropMenu = document.querySelector('.dropdown');
 let inputDrop = document.querySelector('.base-dropdown__arrow');
+let menu = document.querySelector(".dropdown__menu-list");
 
 inputDrop.addEventListener('click', function(evt){
     evt.preventDefault();
     dropMenu.classList.toggle('show');
 });
+
+menu.onclick = function(evt) {
+    let t = evt.target;
+    if (t.className == 'input-num__btn') {
+        let c = t.closest('.input-num');
+        console.log(c.className);
+        let i = c.querySelector('.input-num__display');
+        i.value = +i.value + 1;
+    }
+};
