2d19d1719cd3483ce85e6c6a0177e5b42e6ee4b3
Ivan Meshcheryakov <meshcheryakovvrn@gmail.com>
2020-08-28 03:24:45 +0300

    Add new functionality for dropdown
    Min and Max buttons works properly now. The text of the input changes depending on the number of people.

--- src/common.blocks/dropdown/dropdown.js
+++ src/common.blocks/dropdown/dropdown.js
@@ -1,6 +1,10 @@
 let dropMenu = document.querySelector('.dropdown');
+let inputField = dropMenu.querySelector('.base-field__input');
+let inputFieldDefualValue = 'Сколько гостей';
 let inputDrop = document.querySelector('.base-dropdown__arrow');
-let menu = document.querySelector(".dropdown__menu-list");
+let menu = document.querySelector(".dropdown__menu");
+let resetBtn = menu.querySelector('.dropdown__reset-btn');
+let applyBtn = menu.querySelector('.dropdown__apply-btn');
 
 inputDrop.addEventListener('click', function(evt){
     evt.preventDefault();
@@ -8,11 +12,82 @@ inputDrop.addEventListener('click', function(evt){
 });
 
 menu.onclick = function(evt) {
-    let t = evt.target;
-    if (t.className == 'input-num__btn') {
-        let c = t.closest('.input-num');
-        console.log(c.className);
-        let i = c.querySelector('.input-num__display');
-        i.value = +i.value + 1;
+    let target = evt.target;
+    let displayContainer = target.closest('.input-num');
+    if (displayContainer === null)
+        return;
+    let display = displayContainer.querySelector('.input-num__display');
+
+    if (target.classList.contains('input-num__max-btn')) {
+        if (display.value == 0)
+            displayContainer.querySelector('.input-num__min-btn').classList.remove('input-num__btn_disabled');
+        display.value = +display.value + 1;
+        resetBtn.classList.add('dropdown__reset-btn_visible');
+    }
+    else if (target.classList.contains('input-num__min-btn')) {
+        if (display.value > 0) {
+            display.value = +display.value - 1;
+            if (display.value == 0) {
+                target.classList.add('input-num__btn_disabled');
+            }
+        }
     }
 };
+
+resetBtn.onclick = function(evt) {
+    evt.preventDefault();
+    inputField.value = inputFieldDefualValue;
+    let x = menu.querySelectorAll('.input-num__display');
+    x.forEach(function(x) {
+        x.value = 0;
+    })
+    resetBtn.classList.remove('dropdown__reset-btn_visible');
+}
+
+applyBtn.onclick = function(evt) {
+    evt.preventDefault();
+    let guestSum = sumOfPeople('.dropdown__guests');
+    let babiesSum = sumOfPeople('.dropdown__babies');
+    let guestStr = '';
+    let babiesStr = '';
+    let resStr = 'Сколько гостей';
+
+    if (guestSum > 0)
+        guestStr = +guestSum + abc(guestSum, [' гость', ' гостя', ' гостей']);
+    if (babiesSum > 0) {
+        if (guestSum > 0)
+            babiesStr = ', '; 
+        babiesStr += +babiesSum + abc(babiesSum, [' младенец', ' младенца', ' младенцев']);
+    }
+    
+    resStr = guestStr + babiesStr;
+    inputField.value = resStr;
+}
+
+function sumOfPeople(selector) {
+    let nodes = menu.querySelectorAll(selector);
+    let arr = new Array();
+    let sum = 0;
+
+    nodes.forEach(function(nodes) {
+        arr.push(nodes.querySelector('.input-num__display'));
+    });
+
+    arr.forEach(function(arr) {
+        sum += +arr.value;
+    });
+
+    return sum;
+}
+
+function abc(sum, arr) {
+    let ostatok = sum % 100;
+    let i = 0;
+    if (ostatok > 20)
+        ostatok = ostatok % 10;
+    if (ostatok >= 2 && ostatok <= 4)
+        i = 1;
+    else if ((ostatok >= 5 && ostatok <= 20) || ostatok == 0)
+        i = 2;
+    return arr[i];
+}
--- src/ui-kit.js
+++ src/ui-kit.js
@@ -4,8 +4,8 @@ const Inputmask = require('inputmask');
 const maskedTextField = require('./common.blocks/masked-text-field/masked-text-field.js');
 
 const dropdown = require('./common.blocks/dropdown/dropdown.js');
-const likeButton = require('./common.blocks/like-button/like-button.js');
 const rangeSlider = require('./common.blocks/range-slider/range-slider.js');
 const extCheckList = require('./common.blocks/ext-checkbox-list/ext-checkbox-list.js');
+const likeButton = require('./common.blocks/like-button/like-button.js');
 
 
